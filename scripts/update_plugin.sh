8c45b5804376f6665e4a5966ea8a9eb5eab6aee2
Owen Rumney <owen.rumney@aquasec.com>
2021-12-01 15:21:49 +0000

    Update update_plugin.sh

--- scripts/update_plugin.sh
+++ scripts/update_plugin.sh
@@ -5,11 +5,11 @@ echo "update plugin.yaml"
 sed  -e "s/PLACEHOLDERVERSION/${GITHUB_REF##*/}/g" .github/plugin_template.yaml > plugin.yaml
 
 git fetch --all
-git checkout main
+git checkout master
 git pull
 git config --global user.name "GitHub Actions Build"
 git config --global user.email github-action@aquasec.com
 git add plugin.yaml
 git commit -m "GitHub Actions Build: Update plugin version" || true
-git push --set-upstream origin HEAD:main || true
+git push --set-upstream origin HEAD:master || true
 
