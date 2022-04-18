0e9650a9137dc2fa86f4a60c5b8d4f3d5f43907f
oranmoshai <oran.moshai@aquasec.com>
2022-04-18 16:43:29 +0300

    Bugfix: PR urls was wrong

--- pkg/metadata/pr.go
+++ pkg/metadata/pr.go
@@ -11,16 +11,16 @@ func GetBaseRef() (r string) {
 	switch buildSystem {
 	case azure:
 		return fmt.Sprintf(
-			"base/%s",
+			"origin/%s",
 			strings.ReplaceAll(os.Getenv("SYSTEM_PULLREQUEST_TARGETBRANCH"), "refs/heads/", ""))
 	case bitbucket:
 		return os.Getenv("BITBUCKET_PR_DESTINATION_COMMIT")
 	case github:
-		return os.Getenv("FETCH_HEAD")
+		return "FETCH_HEAD"
 	case gitlab:
 		return os.Getenv("CI_MERGE_REQUEST_DIFF_BASE_SHA")
 	case jenkins:
-		return os.Getenv("CHANGE_TARGET")
+		return fmt.Sprintf("origin/%s", os.Getenv("CHANGE_TARGET"))
 	default:
 		return "origin/master"
 	}
