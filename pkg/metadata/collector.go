c63b75a875a4c3bfc0ab44025752ed43ca8ed02c
Guy Ben-Aharon <baguy3@gmail.com>
2022-07-04 11:45:57 +0300

    removed ".git" from regex since we remove ".git" suffix in GetScmID (#146)
    * fix override branch env value
    
    * removed ".git" from regex since we remove ".git" suffix in GetScmID

--- pkg/metadata/collector.go
+++ pkg/metadata/collector.go
@@ -133,7 +133,7 @@ func getFsRepositoryDetails(scanPath string) (repoName, branch string, err error
 	}
 
 	inferredRepoName := filepath.Base(abs)
-	repoRegex := regexp.MustCompile(`^(?i).+[:/](.+/.+)\.git`)
+	repoRegex := regexp.MustCompile(`^(?i).+[:/](.+/.+)`)
 	scmID, err := GetScmID(scanPath)
 	if err != nil {
 		return inferredRepoName, "", err
