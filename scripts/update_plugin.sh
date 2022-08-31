3210b7ba9a9c097202edc74aba2c94d129dbfa5b
Mor Weinberger <morwnbrg@gmail.com>
2022-08-31 14:06:08 +0300

    update plugin links and create PR as part of the release pipeline

--- scripts/update_plugin.sh
+++ scripts/update_plugin.sh
@@ -17,15 +17,3 @@ fi
 
 git tag -a ${TAG} -m ${TAG}
 git push --tag
-
-
-BRANCH_NAME="plugin-update-${TAG}"
-
-sed  -e "s/PLACEHOLDERVERSION/${TAG}/g" .github/plugin_template.yaml > plugin.yaml
-git checkout -b $BRANCH_NAME
-
-git add plugin.yaml
-git commit -m "Updating plugin to latest tag ${TAG}" || true
-git push --set-upstream origin $BRANCH_NAME || true
-
-xdg-open "https://github.com/aquasecurity/trivy-plugin-aqua/compare/${BRANCH_NAME}"
