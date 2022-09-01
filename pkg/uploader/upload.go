a3736614c59ad4e91465acf7374161d996c92211
‪Tzuriel Weisberg‬‏ <tzubielo@gmail.com>
2022-09-01 15:14:32 +0300

    feat(DEPENDENCIES): Add dependencies from trivy to create request (#207)
    feat(DEPENDENCIES): Add dependencies from trivy to create scan request

--- pkg/uploader/upload.go
+++ pkg/uploader/upload.go
@@ -7,7 +7,12 @@ import (
 )
 
 // Upload forwards the results to the configured client
-func Upload(client buildClient.Client, results []*buildsecurity.Result, tags map[string]string, avdUrlMap buildClient.ResultIdToUrlMap, pipelines []*buildsecurity.Pipeline) error {
+func Upload(client buildClient.Client,
+	results []*buildsecurity.Result,
+	tags map[string]string,
+	avdUrlMap buildClient.ResultIdToUrlMap,
+	pipelines []*buildsecurity.Pipeline,
+	dependencies map[string]*buildsecurity.PackageDependencies) error {
 	log.Logger.Debugf("Uploading scan with tags. %v", tags)
-	return client.Upload(results, tags, avdUrlMap, pipelines)
+	return client.Upload(results, tags, avdUrlMap, pipelines, dependencies)
 }
