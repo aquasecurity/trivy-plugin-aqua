0f2e872fa7303c836ed42f09e7885d562f917cff
‪Tzuriel Weisberg‬‏ <tzubielo@gmail.com>
2022-09-12 09:49:55 +0300

    api url fix (#260)

--- pkg/buildClient/client.go
+++ pkg/buildClient/client.go
@@ -98,7 +98,7 @@ func getCspmAndAquaUrl() (string, string) {
 
 	// New APIs need supply-chain included in prefix instead of api.
 	if urlPrefix != "" {
-		urlPrefix += "supply-chain."
+		urlPrefix += "supply-chain.cloud."
 	} else {
 		urlPrefix = "api."
 	}
