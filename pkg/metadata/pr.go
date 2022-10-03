0c4a976504352db3b034584b1b5170a1cc3bd46a
Lior Vaisman <lior@argon.io>
2022-10-03 16:25:58 +0300

    typo

--- pkg/metadata/pr.go
+++ pkg/metadata/pr.go
@@ -30,7 +30,7 @@ func GetFullBranchName(branchName, remoteFallback string) string {
 	branchPattern := fmt.Sprintf("*/%s", branchName)
 	branch := fmt.Sprintf("%s/%s", remoteFallback, branchName)
 	out, err := git.GitExec("branch", "-a", "--list", branchPattern, "--format=%(refname:lstrip=-2)", "--sort=-refname")
-	if err == nil {
+	if err != nil {
 		if strings.Contains(err.Error(), "error: unknown option `format") { // git version < 2.7.0
 			log.Logger.Warnf("Git version is too old. Please upgrade to 2.7.0 or newer")
 			log.Logger.Debug("Trying to get branch name with old git version")
