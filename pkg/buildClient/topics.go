73c9e6079f72736abaec7609efff1527152ce102
Ori <orizerahmc@gmail.com>
2022-10-03 18:13:28 +0300

    Fixed linter

--- pkg/buildClient/topics.go
+++ pkg/buildClient/topics.go
@@ -6,21 +6,18 @@ import (
 )
 
 func getTopics(buildSystem string) ([]string, error) {
-	switch buildSystem {
-	case metadata.Github:
-		{
-			owner, repo, err := getGitHubRepositoryDetails()
-			if err != nil {
-				return nil, err
-			}
-
-			client := scm.GetGitHubClient()
-			topics, _, err := client.ListRepositoryTopics(owner, repo)
-			if err != nil {
-				return nil, err
-			}
-			return topics, nil
+	if buildSystem == metadata.Github {
+		owner, repo, err := getGitHubRepositoryDetails()
+		if err != nil {
+			return nil, err
 		}
+
+		client := scm.GetGitHubClient()
+		topics, _, err := client.ListRepositoryTopics(owner, repo)
+		if err != nil {
+			return nil, err
+		}
+		return topics, nil
 	}
 	return []string{}, nil
 }
