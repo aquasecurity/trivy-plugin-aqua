a13a1ea874d19d4236c44ab38ed24551e1f4b11d
tzurielw <tzubielo@gmail.com>
2022-10-11 23:18:22 +0300

    test

--- pkg/scanner/diff.go
+++ pkg/scanner/diff.go
@@ -62,7 +62,8 @@ func writeFile(path, content string) error {
 // Create folders with head and base for diff scanning
 func createDiffScanFs(envconfig *models.Configuration) error {
 	// In GitHub we need fetch the remote branch first
-	if envconfig.Repository.Source == enums.Github {
+	log.Logger.Debugf("source is %s", envconfig.Repository.Source)
+	if envconfig.Repository.Source == enums.Github || envconfig.Repository.Source == enums.GithubServer {
 		// In GitHub trivy action container we need safe directory to run git fetch
 		_, err := git.GitExec("config", "--global", "--add", "safe.directory", "/github/workspace")
 		if err != nil {
