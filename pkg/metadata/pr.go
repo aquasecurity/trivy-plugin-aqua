0601b4aec108dd6d7bfb8508dcbd75a0927933ef
‪Tzuriel Weisberg‬‏ <tzubielo@gmail.com>
2022-10-12 15:47:51 +0300

    logs (#326)
    * logs
    
    * logs

--- pkg/metadata/pr.go
+++ pkg/metadata/pr.go
@@ -31,6 +31,7 @@ func GetFullBranchName(branchName, remoteFallback string) string {
 	branch := fmt.Sprintf("%s/%s", remoteFallback, branchName)
 	out, err := git.GitExec("branch", "-a", "--list", branchPattern, "--format=%(refname:lstrip=-2)", "--sort=-refname")
 	if err != nil {
+		log.Logger.Info("err is not nil")
 		if strings.Contains(err.Error(), "error: unknown option `format") { // git version < 2.7.0
 			log.Logger.Warnf("Git version is too old. Please upgrade to 2.7.0 or newer")
 			log.Logger.Debug("Trying to get branch name with old git version")
